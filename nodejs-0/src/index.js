d0b56a2939f87f7072cc0b4028a96a6bfe71c82b
Isabella <isabella.soares.28@gmail.com>
2020-04-25 16:24:23 -0300

    Finish challenge

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,8 +1,23 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+    let a = 0, b = 1, temp = 0;
+    let result = [];
 
-const isFibonnaci = (num) => null
+    while (isFibonnaci(a)) {
+        result.push(a);
+        temp = a;
+        a += b;
+        b = temp;
+    }
+
+    return result;
+}
+
+const isFibonnaci = (num) => {
+    const fibonnaciSequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
+    return fibonnaciSequence.includes(num);
+}
 
 module.exports = {
     fibonacci,
