fd622c25f83bb7e86f9cc3a1637440b58ed6d6a1
Isabella <isabella.soares.28@gmail.com>
2020-05-27 23:01:12 -0300

    Add Fifth Module's Challenge

---
+++ react-13/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Contact.scss';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <article data-testid="contact" className="contact">
+        <span className="contact__avatar" />
+        <span className="contact__data">Nome</span>
+        <span className="contact__data">Telefone</span>
+        <span className="contact__data">País</span>
+        <span className="contact__data">Admissão</span>
+        <span className="contact__data">Empresa</span>
+        <span className="contact__data">Departamento</span>
+      </article>
+    );
+  }
+}
+
+export default Contact;
