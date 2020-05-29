152c584184ef6170fe3455c7c66d7d6d16d2c0b8
IsabellaSoares <isabella.soares.28@gmail.com>
2020-05-29 16:55:42 -0300

    Add last files

--- react-14/src/components/Contacts.jsx
+++ react-14/src/components/Contacts.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react';
+import Contact from './Contact';
+import './Contacts.scss';
 
 class Contacts extends React.Component {
-	render() {
-		return (<div />);
-	}
+  render() {
+    return (
+      <div data-testid="contacts" className="container">
+        <section className="contacts">
+          <Contact />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Contacts;
