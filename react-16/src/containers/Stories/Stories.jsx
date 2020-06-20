ba7e8ed380ccb9043a4e07843f1afa120bc51176
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-20 11:47:47 -0300

    feat: add stories

--- react-16/src/containers/Stories/Stories.jsx
+++ react-16/src/containers/Stories/Stories.jsx
@@ -5,15 +5,52 @@ import Story from '../../components/Story';
 import './Stories.scss';
 
 const Stories = ({ stories, getUserHandler }) => {
-  const [showStory, setShowStory] = useState(true);
+  const [showStory, toggleShowStory] = useState(false);
+  const [selectedStory, setSelectedHistory] = useState({});
+  const [selectedProfile, setSelectedProfile] = useState({});
+
+  const findStoryById = (storyId) =>
+    stories.find((story) => storyId === story.id);
+
+  const handleClick = (story) => {
+    const foundStory = findStoryById(story.id);
+    const profile = getUserHandler(story.userId);
+
+    setSelectedHistory(foundStory);
+    setSelectedProfile(profile);
+    toggleShowStory(!showStory);
+  };
 
   return (
     <React.Fragment>
       <section data-testid="stories" className="stories">
-        <div className="container"></div>
+        <div className="container">
+          {stories.map((story, index) => {
+            const profile = getUserHandler(story.userId);
+
+            return (
+              <button
+                className={`user__thumb ${
+                  index === 0 && 'user__thumb--hasNew'
+                }`}
+                onClick={() => handleClick(story)}
+              >
+                <div className="user__thumb__wrapper">
+                  <img src={profile.avatar} alt={profile.name} />
+                </div>
+              </button>
+            );
+          })}
+        </div>
       </section>
 
-      {showStory && <Story />}
+      {showStory && (
+        <Story
+          story={selectedStory}
+          user={selectedProfile}
+          handleClose={() => toggleShowStory(!showStory)}
+        />
+      )}
     </React.Fragment>
   );
 };
--- react-16/src/routes/FeedRoute/FeedRoute.jsx
+++ react-16/src/routes/FeedRoute/FeedRoute.jsx
@@ -8,7 +8,39 @@ import Posts from '../../containers/Posts';
 import './FeedRoute.scss';
 
 const FeedRoute = () => {
-  return <div data-testid="feed-route"></div>;
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('	https://5e7d0266a917d70016684219.mockapi.io/api/v1/users/:id/posts')
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  });
+
+  useEffect(() => {
+    fetch('	https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories')
+      .then((res) => res.json())
+      .then((data) => setStories(data));
+  }, [users]);
+
+  const getUserById = (userId) => users.find((user) => userId === user.id);
+
+  return (
+    <div data-testid="feed-route">
+      {users.length > 0 && stories.length > 0 && (
+        <Stories stories={stories} getUserHandler={getUserById} />
+      )}
+
+      {/* <Posts posts={posts} /> */}
+    </div>
+  );
 };
 
 export default FeedRoute;
