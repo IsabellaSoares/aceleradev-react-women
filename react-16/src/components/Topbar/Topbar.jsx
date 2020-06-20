3678c39eb1ef4101a085a6691946ceec32742fbb
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-20 09:25:20 -0300

    feat: create topbar

--- react-16/src/components/Topbar/Topbar.jsx
+++ react-16/src/components/Topbar/Topbar.jsx
@@ -8,7 +8,27 @@ import './Topbar.scss';
 
 const Topbar = () => (
   <header data-testid="topbar" className="topbar">
-    <div className="container"></div>
+    <div className="container">
+      <Link to="/" className="topbar__logo">
+        <LogoSvg />
+      </Link>
+
+      <div className="topbar__group">
+        <button className="topbar__icon">
+          <Link to="/users">
+            <i className="fas fa-users"></i>
+            <span>Usuários</span>
+          </Link>
+        </button>
+
+        <button className="topbar__icon">
+          <Link to="/newuser">
+            <i className="fas fa-user-plus"></i>
+            <span>Nova conta</span>
+          </Link>
+        </button>
+      </div>
+    </div>
   </header>
 );
 
