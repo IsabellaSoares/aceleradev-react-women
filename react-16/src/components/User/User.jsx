03b29d0d6de4bfd5fde6842ec0f07658fb15da71
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-20 13:40:10 -0300

    feat: add users page

--- react-16/src/components/User/User.jsx
+++ react-16/src/components/User/User.jsx
@@ -6,8 +6,23 @@ const User = ({ infoUser }) => {
   const { avatar, name, username } = infoUser;
 
   return (
-    <article data-testid="user" className="post">
-      <header className="post__header"></header>
+    <article className="post" data-testid="user">
+      <header className="post__header">
+        <Link to={`/users/${username}`} className="user">
+          <div className="user__thumb">
+            {avatar ? (
+              <img src={avatar} alt={name} />
+            ) : (
+              <img
+                src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/profile-placeholder.png"
+                alt=""
+              />
+            )}
+          </div>
+
+          <div className="user__name">{name}</div>
+        </Link>
+      </header>
     </article>
   );
 };
