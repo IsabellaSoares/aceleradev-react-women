55762671957ec4b8f45c2b8d9a297477ea7c5632
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-20 13:28:03 -0300

    feat: add user form

--- react-16/src/routes/NewUserRoute/NewUserRoute.jsx
+++ react-16/src/routes/NewUserRoute/NewUserRoute.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import UserForm from '../../containers/UserForm';
 
-const NewUserRoute = () => <section data-testid="new-user-route"></section>;
+const NewUserRoute = () => (
+  <section data-testid="new-user-route">
+    <UserForm />
+  </section>
+);
 
 export default NewUserRoute;
