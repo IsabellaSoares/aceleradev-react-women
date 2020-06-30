c16bea43412d8f4ca6ea3b42b1a4e6fb8d120821
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-30 08:20:10 -0300

    feat: add API configs

--- react-17/src/config/index.js
+++ react-17/src/config/index.js
@@ -1,13 +1,9 @@
 export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
-    clientId: '',
+    clientId: 'da63ccc4dff24d2d8de96122ba458a64',
     redirectUrl: `${window.location.origin}/authorize`,
     webAPI: 'https://api.spotify.com/v1',
-    scopes: [
-      'user-read-email',
-      'user-read-private',
-      'streaming'
-    ]
-  }
-}
+    scopes: ['user-read-email', 'user-read-private', 'streaming'],
+  },
+};
